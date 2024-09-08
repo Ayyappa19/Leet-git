95516ed4ffdfa45c5e58064ba08c13547ad99cd4
Ayyappa Swamy Yelugubanti <119914633+Ayyappa19@users.noreply.github.com>
2024-09-08 22:31:20 +0530

    Time: 375 ms (30.49%), Space: 17.6 MB (89.45%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, arr: List[int]) -> bool:
+        n = len(arr)
+        x = y = z = 0
+    
+        for i in range(n-1):
+            x = max(x, arr[i]+i)
+            if i == y:
+                y = x
+                z +=1
+    
+        if y >= n-1:
+            return True
+        return False
+    
+        
